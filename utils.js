06667f4200abb4a1372133c9823f15aec5d8da73
parul-vs <parul.sharma@vectoscalar.com>
2020-08-04 20:33:32 +0530

    Mission Section Added

---
+++ utils.js
@@ -0,0 +1,18 @@
+let backgroundColor = "#0C1D40";
+let color = {
+  primary: "#102A5A",
+  pink: "#E338AA",
+  pinkRed: "#FF2C8C",
+  lightBlue1: "#176898",
+  lightBlue2: "#A5C1E5",
+  white: "#fff",
+};
+let backgroundImage = "url(./images/bg2.jpg)";
+
+// devlope purpose only
+// document.designMode="off";
+
+// document.documentElement.style.setProperty('--main', "green");
+// let cssVariable = getComputedStyle(document.body);
+// alert(cssVariable.getPropertyValue('--main'));
+// console.log(cssVariable);
