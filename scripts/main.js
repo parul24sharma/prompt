b30406439895191883b4947bc272d72c1fa3a627
parul24sharma <psharma.24222@gmail.com>
2020-08-08 22:07:13 +0530

    Question paper section added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+/** Navbar Toggle for mobile phone */
 $(document).ready(function () {
   $(".menu-toggle").click(function () {
     $(".menu-toggle").toggleClass("active");
@@ -10,22 +10,25 @@ $(document).ready(function () {
   });
 });
 
-document.body.style.background = backgroundColor;
+/** main attributes */
 document.querySelector(".bgimage").style.backgroundImage =
-  backgroundImage;
+  "url(./images/bg2.jpg)";
 document.querySelector(".head-text").innerHTML =
   "Welcome! to the Wikipedia ✌ of BVoc";
 
-let mission = [
+/** Mission Section  */
+let missionObject = [
   {
     imageSrc: "./images/light-bulb.gif",
     imageAlt: "Book Gif",
-    missionMessage: "Know Everything about Bachelor of Vocation undergraduate course",
+    missionMessage:
+      "Know Everything about Bachelor of Vocation undergraduate course",
   },
   {
     imageSrc: "./images/book.gif",
     imageAlt: "Light Gif",
-    missionMessage: "Get all the previous year Question Papers and resources for the coursework",
+    missionMessage:
+      "Get all the previous year Question Papers and resources for the coursework",
   },
   {
     imageSrc: "./images/badge.gif",
@@ -34,8 +37,34 @@ let mission = [
   },
 ];
 
-mission.map((element,i)=>{
+missionObject.map((element, i) => {
+  let missionElement = document.createElement("div");
+  missionElement.setAttribute("class", "mission");
+  missionElement.innerHTML = `<img class="mission-image" />
+  <span class="mission-message"></span>`;
+  document.querySelector(".mission-container").appendChild(missionElement);
+
   document.querySelectorAll(".mission > img")[i].src = element.imageSrc;
   document.querySelectorAll(".mission > img")[i].alt = element.imageAlt;
-  document.querySelectorAll(".mission > .mission-message")[i].innerHTML = element.missionMessage;
-})
+  document.querySelectorAll(".mission > .mission-message")[i].innerHTML =
+    element.missionMessage;
+});
+
+let question = [
+  {
+    year : 2018,
+    nameOfPaper : "Management of Information System"
+  },
+  {
+    year : 2017,
+    nameOfPaper : "Computer Fundamentals"
+  },
+  {
+    year : 2015,
+    nameOfPaper : "Communication Competency"
+  },
+  {
+    year : 2016,
+    nameOfPaper : "Computer Application in Communication and Media Design"
+  }
+];
